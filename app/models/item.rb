8f9cd166dfe40972ccfedbd6f0f7f2a9361233ef
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-01-21 21:28:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equel_to: 9999999, message: "is invalid"}
+  end
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank"}
 end
