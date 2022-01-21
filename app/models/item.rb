f108ce3246c3ae99a61c8ad265950556a2e6087d
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-01-21 18:20:18 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
+  has_one_attached :image
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+  belongs_to :user
 end
