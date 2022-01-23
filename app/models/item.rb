3caea9c705169c33e0f1af5f6976b8a74fa2cd06
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-01-23 21:08:55 +0900

    Itemモデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equel_to: 9999999, message: "is invalid"}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
   end
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品情報の保存' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+  
+    context '商品情報を保存できる場合' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品情報を保存できない場合' do
+      it 'imageが空だと保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空だと保存できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'categoryを選択していないと保存できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'sales_statusを選択していないと保存できない' do
+        @item.sales_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it 'shipping_fee_statusを選択していないと保存できない' do
+        @item.shipping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+      it 'prefectureを選択していないと保存できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'scheduled_deliveryを選択していないと保存できない' do
+        @item.scheduled_delivery_id ='1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+      it 'priceが空だと保存できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300円未満だと保存できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priseが9,999,999円を超過すると保存できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが半角数字でないと保存できない' do
+        @item.price = '２０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+    end
+  end
 end
