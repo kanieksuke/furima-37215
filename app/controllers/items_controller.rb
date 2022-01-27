18519c860d243fc82355c44794c99008a5774da7
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-01-27 22:24:07 +0900

    編集後の遷移先を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to item_path
     else
       render :edit
     end
